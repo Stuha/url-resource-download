d5b9c5a22a4b338522df7701faa5ba1deda62422
Sasha Buljevic <sasha_buljevic@hotmail.com>
2022-10-07 15:44:16 +0200

    remove unnessecary classes

--- resources/views/url-content.blade.php
+++ resources/views/url-content.blade.php
@@ -1,6 +1,6 @@
-<div class="container">
+<div>
     @if ($errors->any())
-    <div class="alert alert-danger">
+    <div>
         <ul>
             @foreach ($errors->all() as $error)
             <li>{{ $error }}</li>
@@ -8,31 +8,31 @@
         </ul>
     </div>
     @endif
-    <div class="row">
-        <div class="col-10">
-            <table class="table table-striped">
-                <thead class="table-header fixed-header">
-                    <tr class="table-warning">
-                        <th class="text-center">Id</th>
-                        <th class="text-center">Filename</th>
-                        <th class="text-center">Url</th>
-                        <th class="text-center">Status</th>
+    <div>
+        <div>
+            <table>
+                <thead>
+                    <tr>
+                        <th>Id</th>
+                        <th>Filename</th>
+                        <th>Url</th>
+                        <th>Status</th>
                     </tr>
                 </thead>
                 <tbody>
                     @foreach ($contents as $content)
                     <tr>
-                        <td class="text-center" title="Id">{{ $content->id }}.</td>
-                        <td class="text-center" title="Filename">{{ $content->filename }}</td>
+                        <td title="Id">{{ $content->id }}.</td>
+                        <td title="Filename">{{ $content->filename }}</td>
                         @if(\App\Enums\Status::COMPLETE === $content->status)
-                        <td class="text-center" title="Url">
+                        <td title="Url">
                             <a href="{{ route('download-content', ['id' => $content->id, 'filename' => $content->filename]) }}">
                                 {{ $content->url }}</a>
                         </td>
                         @else
-                        <td class="text-center" title="Url">{{ $content->url }}</td>
+                        <td title="Url">{{ $content->url }}</td>
                         @endif
-                        <td class="text-center" title="Status">{{ $content->status }}</td>
+                        <td title="Status">{{ $content->status }}</td>
                     </tr>
                     @endforeach
 
